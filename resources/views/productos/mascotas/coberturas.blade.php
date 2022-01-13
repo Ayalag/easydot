cf8e9d3bd265f2ae597ad135730dfe7b7f984e1e
Gadiel <gayala@aswgr.com>
2022-01-13 17:56:12 -0500

    vet3

---
+++ resources/views/productos/mascotas/coberturas.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="container-fluid container-cobertura pt-5">
+        <div class="container container-button">
+            <div class="box-bottom pt-5" id="veterinario">Seguros veterinarios</div>
+        </div>
+
+    </div>
+
+@endsection
+
+@section('scripts')
+
+    <script>
+        /*-------------------------------*/
+        /*        Cobertura              */
+        /*-------------------------------*/
+        document.querySelector('#veterinario').addEventListener('click', function(e) {
+            location.href = "/producto/personas/er";
+        });
+    </script>
+
+@endsection
